a01156096d0beef4e2d3a684344524426649f1a0
Jeremy <kwagh.jeremiah@gmail.com>
2024-03-06 12:40:28 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+import path from 'path';
+
 export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080
+  },
   plugins: [react()],
-})
+});
